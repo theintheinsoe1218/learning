87a7d645fd353855e14fa2333f0e14bbf56bb1b1
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-12-14 15:27:05 +0630

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.MINUS:
+      return { ...state, count: state.count - 1 };
+    case ACTION.PLUS:
+      return { ...state, count: state.count + 1 };
+    case ACTION.UPDATE_KEY:
+      return { ...state, key: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const ACTION = {
+  PLUS: "plus",
+  MINUS: "minus",
+  UPDATE_KEY: "updatekey",
+};
 function App() {
-  const [key, setKey] = useState("");
-  useEffect(() => {
-    console.log("i am useEffect");
-  }, []);
+  // const [key, setKey] = useState("");
+  // const [count, setCount] = useState(0);
+
+  const [state, dispatch] = useReducer(firstReducer, { count: 0, key: "" });
+
   return (
     <div className="App">
-      <h1>Hello useEffect</h1>
+      <h1>Hello useReducer</h1>
       <input
         type="text"
-        onChange={(e) => {
-          setKey(e.target.value);
-        }}
+        onChange={(e) =>
+          dispatch({ type: ACTION.UPDATE_KEY, payload: e.target.value })
+        }
       />
+
+      <h2>Your key is - {state.key}</h2>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION.MINUS });
+        }}
+      >
+        -
+      </button>
+      <span>{state.count}</span>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION.PLUS });
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
