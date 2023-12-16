d77fc0ff7bfe3b11c78feb3a6890848e4aeb34b5
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-12-16 11:45:46 +0630

    fetch api with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,39 @@
-import { useEffect, useReducer, useState } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const firstReducer = (state, action) => {
-  switch (action.type) {
-    case ACTION.MINUS:
-      return { ...state, count: state.count - 1 };
-    case ACTION.PLUS:
-      return { ...state, count: state.count + 1 };
-    case ACTION.UPDATE_KEY:
-      return { ...state, key: action.payload };
-    default:
-      throw new Error();
-  }
-};
-
-const ACTION = {
-  PLUS: "plus",
-  MINUS: "minus",
-  UPDATE_KEY: "updatekey",
-};
 function App() {
-  // const [key, setKey] = useState("");
-  // const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const [state, dispatch] = useReducer(firstReducer, { count: 0, key: "" });
-
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await response.json();
+    setTodos(data);
+  };
   return (
     <div className="App">
-      <h1>Hello useReducer</h1>
-      <input
-        type="text"
-        onChange={(e) =>
-          dispatch({ type: ACTION.UPDATE_KEY, payload: e.target.value })
-        }
-      />
+      <h1>Fetch Api Using UseEffect</h1>
 
-      <h2>Your key is - {state.key}</h2>
-      <button
-        onClick={() => {
-          dispatch({ type: ACTION.MINUS });
-        }}
-      >
-        -
-      </button>
-      <span>{state.count}</span>
-      <button
-        onClick={() => {
-          dispatch({ type: ACTION.PLUS });
-        }}
-      >
-        +
-      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.title}</td>
+              <td>{todo.completed ? <p>Done</p> : <p>Not Complete</p>}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
