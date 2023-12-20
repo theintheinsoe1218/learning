26112bd0c2f1eb28aa9d2976b05398e84c24c218
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-12-20 11:03:28 +0630

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,17 @@
-import { useState } from "react";
+import Child1 from "./components/Child1";
+import Child2 from "./components/Child2";
+import Child3 from "./components/Child3";
+import FirstContextProvider from "./components/context/FirstContext";
 
 function App() {
-  const [id, setId] = useState("");
-  const [error, setError] = useState(false);
-  const [todo, setTodo] = useState(null);
-  const getId = async (e) => {
-    e.preventDefault();
-
-    if (id < 1) {
-      setError(true);
-      setId("");
-      return;
-    }
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/todos/${id}`
-    );
-    const data = await response.json();
-    setError(false);
-    setTodo(data);
-
-    setId("");
-  };
   return (
     <div className="App">
-      <h1>Fetch Api Using UseEffect With Input </h1>
-
-      <form onSubmit={getId}>
-        <input
-          type="number"
-          name=""
-          value={id}
-          onChange={(e) => {
-            setId(e.target.value);
-          }}
-        />
-        <button type="submit">Get Data</button>
-      </form>
-      <div>
-        {error && <h1>Please Enter an valid id. (eg. 1,2,3,...)</h1>}
-        {todo && (
-          <div key={todo.id}>
-            <h1>title - {todo.title}</h1>
-            <h1>userId - {todo.userId}</h1>
-            <h1>
-              completed -{" "}
-              {todo.completed ? (
-                <span>Completed</span>
-              ) : (
-                <span>Not Complete</span>
-              )}
-            </h1>
-          </div>
-        )}
-      </div>
+      <h1>Fetch Api Using useContext</h1>
+      <FirstContextProvider>
+        <Child1 />
+        <Child2 />
+        <Child3 />
+      </FirstContextProvider>
     </div>
   );
 }
---
+++ src/components/Child2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Child2 = (props) => {
+  return <div>Child2</div>;
+};
+
+export default Child2;
